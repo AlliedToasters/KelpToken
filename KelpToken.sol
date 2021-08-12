44e617561cfa7f4e24e070cbcda4b46d9b138052
Michael Klear <michael.klear@nike.com>
2021-08-11 20:00:49 -0700

    first commit

---
+++ KelpToken.sol
@@ -0,0 +1,38 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract KelpToken is Ownable, ERC20 {
+    
+    using SafeMath for uint256;
+    uint256 public totalGramsBiomassSequestered;
+    uint256 public appreciationParameter;
+    
+    constructor(uint256 initialGramsBioMassSequestered) ERC20("KelpToken", "KELP") {
+        appreciationParameter = 1e11;
+        totalGramsBiomassSequestered = totalGramsBiomassSequestered.add(initialGramsBioMassSequestered);
+        _mint(msg.sender, _computeAmountToMint(initialGramsBioMassSequestered));
+    }
+    
+    function _computeAmountToMint(uint256 _bioMassGrams) private view returns (uint256) {
+        return (_bioMassGrams.mul(1e8)).div(((totalSupply().div(appreciationParameter)).add(1)));
+    }
+    
+    function decimals() public view virtual override returns (uint8) {
+      return 8;
+    }
+    
+    function setAppreciationParameter(uint _value) public onlyOwner {
+        appreciationParameter = _value;
+    }
+    
+    function mintKelpToken(address _to, uint _gramsBioMassSequestered) public onlyOwner {
+        totalGramsBiomassSequestered = totalGramsBiomassSequestered.add(_gramsBioMassSequestered);
+        _mint(_to, _computeAmountToMint(_gramsBioMassSequestered));
+    }
+    
+}
