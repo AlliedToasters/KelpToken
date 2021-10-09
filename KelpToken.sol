5929c70c3abcd63c29751fceab400b6712d184b0
Michael Klear <michael.klear@nike.com>
2021-10-09 11:29:31 -0700

    update stuff

--- KelpToken.sol
+++ KelpToken.sol
@@ -2,37 +2,55 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "openzeppelin-solidity/contracts/access/Ownable.sol";
+import "openzeppelin-solidity/contracts/utils/math/SafeMath.sol";
 
 contract KelpToken is Ownable, ERC20 {
     
     using SafeMath for uint256;
-    uint256 public totalGramsBiomassSequestered;
-    uint256 public appreciationParameter;
+    uint256 public totalGramsCarbonSequestered;
+    uint256 public liquidityParameter;
     
-    constructor(uint256 initialGramsBioMassSequestered) ERC20("KelpToken", "KELP") {
-        appreciationParameter = 5e12; //higher value means more circulating supply
-        totalGramsBiomassSequestered = totalGramsBiomassSequestered.add(initialGramsBioMassSequestered);
-        _mint(msg.sender, computeAmountToMint(initialGramsBioMassSequestered));
+    event KelpMinted(
+        uint _gramsCarbonSequestered, 
+        uint _unitsMinted, 
+        uint _issuancePlanktonPerGram,
+        uint _cumulativeMassGrams,
+        uint _cumulativeSupply,
+        bytes32 _docHash,
+        address _beneficiary
+        );
+    
+    constructor(uint256 _liquidityParameter) ERC20("KelpToken", "KELP") {
+        liquidityParameter = _liquidityParameter; //higher value means more circulating supply
     }
     
-    function computeAmountToMint(uint256 _bioMassGrams) public view returns (uint256) {
-        return (_bioMassGrams.mul(1e8)).div(((totalSupply().div(appreciationParameter)).add(1)));
+    function computeAmountToMint(uint256 _carbonGrams) public view returns (uint256) {
+        return (_carbonGrams.mul(1e8)).div(((totalSupply().div(liquidityParameter)).add(1)));
     }
     
     function decimals() public view virtual override returns (uint8) {
       return 8;
     }
     
-    function setAppreciationParameter(uint _value) public onlyOwner {
-        appreciationParameter = _value;
+    function setliquidityParameter(uint _value) public onlyOwner {
+        liquidityParameter = _value;
     }
     
-    function mintKelpToken(address _to, uint _gramsBioMassSequestered) public onlyOwner {
-        totalGramsBiomassSequestered = totalGramsBiomassSequestered.add(_gramsBioMassSequestered);
-        _mint(_to, computeAmountToMint(_gramsBioMassSequestered));
+    function mintKelpToken(address _to, uint _gramsCarbonSequestered, bytes32 _docHash) public onlyOwner {
+        totalGramsCarbonSequestered = totalGramsCarbonSequestered.add(_gramsCarbonSequestered);
+        uint amountToMint = computeAmountToMint(_gramsCarbonSequestered);
+        emit KelpMinted(
+            _gramsCarbonSequestered, 
+            amountToMint, 
+            amountToMint.div(_gramsCarbonSequestered),
+            totalGramsCarbonSequestered,
+            totalSupply().add(amountToMint),
+            _docHash,
+            _to
+            );
+        _mint(_to, amountToMint);
     }
     
 }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # KelpToken
+
 WIP
+
+## Contract Methods
+
+### ERC20 Standard Methods
+Built using [OpenZeppelin's ERC20 implementation](https://docs.openzeppelin.com/contracts/2.x/api/token/erc20). Please see their docs for the ERC20 standard methods.
+
+### Custom Methods
+The KelpToken contract implements a few new methods on top of ERC20 and overrides a few.
+
+```
+    event KelpMinted(
+        uint _gramsCarbonSequestered, 
+        uint _unitsMinted, 
+        uint _issuancePlanktonPerGram,
+        uint _cumulativeMassGrams,
+        uint _cumulativeSupply,
+        bytes32 _docHash,
+        address _beneficiary
+        );
+```
